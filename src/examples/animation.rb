4a629c17575999224b91ea99ebdac1f9c24a2e46
mamoru-yuo <mamoru-yuo@88017901-6811-4be7-9f05-d6f8af9ca68b>
2012-06-01 08:17:26 +0000

    Modify: Modify examples

--- src/examples/animation.rb
+++ src/examples/animation.rb
@@ -3,26 +3,102 @@
 require 'rubygems'
 require 'dyi'
 
-canvas = DYI::Canvas.new(400, 300)
+canvas = DYI::Canvas.new(250, 140)
+canvas.title = 'Animation Icons without Script'
+canvas.description = 'This image is a sample of DYI which uses SVG animation.'
 
-rect = DYI::Drawing::Brush.new(:color=>'#51ADE2').draw_rectangle(canvas, [10,10], 300, 200)
-rect.add_painting_animation(:from => {:fill => '#51ADE2'},
-                            :to => {:fill => 'red'},
-                            :duration => 3,
-                            :begin_event => DYI::Event.mouseover(rect),
-                            :end_event => DYI::Event.mouseout(rect),
-                            :fill => 'freeze')
-text = DYI::Drawing::Brush.new.draw_text(canvas,
-                                         [100,250],
-                                         'click me!',
-                                         :show_border => true,
-                                         :border_color=>'#325BA8',
-                                         :padding => 8,
-                                         :border_rx => 10,
-                                         :border_width => 3,
-                                         :background_color=>'#A5C7F8')
-rect.add_painting_animation(:to => {:fill => 'green'},
-                            :begin_event => DYI::Event.click(text),
-                            :fill => 'freeze')
+# Left-Hand Icon
+icon1 = DYI::Shape::ShapeGroup.draw_on(canvas)
+pen = DYI::Drawing::Pen.new(:stroke_width => 6, :stroke_linecap => 'round')
+pen.color = '#DDD'
+line = pen.draw_line(icon1, [10,0], [18,0])
+line.add_painting_animation(:from => {:stroke => '#DDD'},
+                            :relays => [{:stroke => '#DDD'}, {:stroke => '#333'}, {:stroke => '#DDD'}],
+                            :to => {:stroke => '#DDD'},
+                            :relay_times => [0.125, 0.125, 0.75],
+                            :duration => 1,
+                            :repeat_count => 0)
+line = pen.draw_line(icon1, [10,0], [18,0])
+line.rotate(45)
+line.add_painting_animation(:from => {:stroke => '#DDD'},
+                            :relays => [{:stroke => '#DDD'}, {:stroke => '#333'}, {:stroke => '#DDD'}],
+                            :to => {:stroke => '#DDD'},
+                            :relay_times => [0.25, 0.25, 0.825],
+                            :duration => 1,
+                            :repeat_count => 0)
+line = pen.draw_line(icon1, [10,0], [18,0])
+line.rotate(90)
+line.add_painting_animation(:from => {:stroke => '#DDD'},
+                            :relays => [{:stroke => '#DDD'}, {:stroke => '#333'}],
+                            :to => {:stroke => '#DDD'},
+                            :relay_times => [0.375, 0.375, 1],
+                            :duration => 1,
+                            :repeat_count => 0)
+line = pen.draw_line(icon1, [10,0], [18,0])
+line.rotate(135)
+line.add_painting_animation(:from => {:stroke => '#BBB'},
+                            :relays => [{:stroke => '#DDD'}, {:stroke => '#DDD'}, {:stroke => '#333'}],
+                            :to => {:stroke => '#BBB'},
+                            :relay_times => [0.125, 0.5, 0.5],
+                            :duration => 1,
+                            :repeat_count => 0)
+pen.color = '#999'
+line = pen.draw_line(icon1, [10,0], [18,0])
+line.rotate(180)
+line.add_painting_animation(:from => {:stroke => '#999'},
+                            :relays => [{:stroke => '#DDD'}, {:stroke => '#DDD'}, {:stroke => '#333'}],
+                            :to => {:stroke => '#999'},
+                            :relay_times => [0.25, 0.625, 0.625],
+                            :duration => 1,
+                            :repeat_count => 0)
+pen.color = '#777'
+line = pen.draw_line(icon1, [10,0], [18,0])
+line.rotate(225)
+line.add_painting_animation(:from => {:stroke => '#777'},
+                            :relays => [{:stroke => '#DDD'}, {:stroke => '#DDD'}, {:stroke => '#333'}],
+                            :to => {:stroke => '#777'},
+                            :relay_times => [0.375, 0.75, 0.75],
+                            :duration => 1,
+                            :repeat_count => 0)
+pen.color = '#555'
+line = pen.draw_line(icon1, [10,0], [18,0])
+line.rotate(270)
+line.add_painting_animation(:from => {:stroke => '#555'},
+                            :relays => [{:stroke => '#DDD'}, {:stroke => '#DDD'}, {:stroke => '#333'}],
+                            :to => {:stroke => '#555'},
+                            :relay_times => [0.5, 0.875, 0.875],
+                            :duration => 1,
+                            :repeat_count => 0)
+pen.color = '#333'
+line = pen.draw_line(icon1, [10,0], [18,0])
+line.rotate(315)
+line.add_painting_animation(:from => {:stroke => '#333'},
+                            :relays => [{:stroke => '#DDD'}, {:stroke => '#DDD'}],
+                            :to => {:stroke => '#333'},
+                            :relay_times => [0.625, 1],
+                            :duration => 1,
+                            :repeat_count => 0)
+icon1.translate(70,70)
+
+# Right-Hand Icon
+icon2 = DYI::Shape::ShapeGroup.draw_on(canvas)
+brush = DYI::Drawing::Brush.new(:color => '#08A')
+brush.draw_closed_path(icon2, [-14.489, -3.882]){|path|
+  path.arc_to([12.99, -7.5], 15, 15)
+  path.line_to([17.321, -10], [14.5, 0], [4.33, -2.5], [8.66, -5])
+  path.arc_to([-9.397, -3.42], 10, 10, 0, false, false)
+}
+brush.draw_closed_path(icon2, [14.489, 3.882]){|path|
+  path.arc_to([-12.99, 7.5], 15, 15)
+  path.line_to([-17.321, 10], [-14.5, 0], [-4.33, 2.5], [-8.66, 5])
+  path.arc_to([9.397, 3.42], 10, 10, 0, false, false)
+}
+icon2.add_transform_animation(:rotate,
+                              :from => 0,
+                              :to => 360,
+                              :duration => 1.5,
+                              :repeat_count => 0,
+                              :additive => 'sum')
+icon2.translate(180,70)
 
 canvas.save('output/animation.svg')
--- src/examples/pie_chart.rb
+++ src/examples/pie_chart.rb
@@ -3,25 +3,32 @@
 require 'rubygems'
 require 'dyi'
 
-chart = DYI::Chart::PieChart.new 400,500,
-  :data_label_format => "{?name}\n{?percent}",
-  :data_label_font => {:size => 20, :font_family=>'Serif'},
-  :represent_3d => true,
-  :show_data_label => false,
-  :show_baloon => true,
-  :baloon_opacity => 0.3,
-  :baloon_format => "{?name}\n{?percent}",
-#  :moved_elements => [0.2,nil,nil,0.5],
-#  :chart_colors => ['blue', 'red', 'yellow', 'green'],
-  :_3d_settings => {:dy => 30},
-  :background_image_opacity => 0.1,
-  :legend_format => "{?name}\t{!e}{?value:#,##0}\t{!e}({?percent:0.00%})",
-  :chart_stroke_color => 'white'
+# Nominal GDP of Asian Countries (2010)
+chart_data = [['China', 5878],
+              ['Japan', 5459],
+              ['India', 1538],
+              ['South Koria', 1007],
+              ['Other Countries', 2863]]
+reader = DYI::Chart::ArrayReader.read(chart_data, :schema => [:name, :value])
 
-reader = DYI::Chart::ExcelReader.read('data/currency.xlsx', :schema => [:name, :value])
-chart.load_data reader
+# Creates the Pie Chart
+chart = DYI::Chart::PieChart.new(500,240,
+          :center_point => [130, 130],
+          :legend_point => [250, 80],
+          :chart_radius_x => 100,
+          :show_data_label => false,
+          :represent_3d => true,
+          :_3d_settings => {:dy => 20},
+          :baloon_format => "{?name}\n{?value:#,0}",
+          :legend_format => "{?name}\t{!e}{?value:#,0}\t{!e}({?percent:0.0%})",
+          :chart_stroke_color => 'white',
+          :background_image_file => {:path => 'external_files/asian_map.png', :content_type => 'image/png'},
+          :background_image_opacity => 0.3)
+chart.load_data(reader)
+DYI::Drawing::Pen.black_pen.draw_text(chart.canvas,
+          [250, 40],
+          'Nominal GDP of Asian Countries (2010)',
+          :text_anchor => 'middle',
+          :font => {:font_family => 'sans-serif', :size => '14pt', :weight => 'bold'})
 
-chart.save 'output/pie_chart.svg'
-chart.save 'output/pie_chart.xaml', :xaml
-chart.save 'output/pie_chart.eps', :eps
-chart.save 'output/pie_chart.emf', :emf if defined? IRONRUBY_VERSION
+chart.save('output/pie_chart.svg')
